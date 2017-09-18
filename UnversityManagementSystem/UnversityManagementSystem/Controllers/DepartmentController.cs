07e9b82997a3b121deb6c8f8ad4cfc84cf889c29
github_Shihab09 <shihab9rasim@gmail.com>
2017-09-18 22:35:06 +0600

    complete

---
+++ UnversityManagementSystem/UnversityManagementSystem/Controllers/DepartmentController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UnversityManagementSystem.Models;
+
+namespace UnversityManagementSystem.Controllers
+{
+    public class DepartmentController : Controller
+    {
+      
+        private ProjectDb db = new ProjectDb();
+        
+        // GET: /Department/
+        public ActionResult Index()
+        {
+            return View(db.Departments.ToList());
+        }
+        //check departmemt is available
+        public JsonResult IsDeptCodeAvailable(string DeptCode)
+        {
+            return Json(!db.Departments.Any(x => x.DeptCode == DeptCode),
+                                                 JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult IsDeptNameAvailable(string DeptName)
+        {
+            return Json(!db.Departments.Any(x => x.DeptName == DeptName),
+                                                 JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: /Department/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = db.Departments.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // GET: /Department/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Department/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="DepartmentId,DeptCode,DeptName")] Department department)
+        {
+         
+            if (ModelState.IsValid)
+            {
+                db.Departments.Add(department);
+                db.SaveChanges();
+                TempData["Success"] = "Department Successfully Saved!";
+                return RedirectToAction("Create");
+                //return RedirectToAction("Index");
+            }
+
+            
+            return View(department);
+        }
+
+        // GET: /Department/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = db.Departments.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // POST: /Department/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="DepartmentId,DeptCode,DeptName")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(department).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(department);
+        }
+
+        // GET: /Department/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = db.Departments.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // POST: /Department/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Department department = db.Departments.Find(id);
+            db.Departments.Remove(department);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+        
+
+    }
+}
